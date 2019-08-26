5727bc1bd53796efd7475ae8307216d0b8d20e69
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-26 12:15:49 -0600

    Set up auth router

---
+++ auth/auth-router.js
@@ -0,0 +1,56 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const Users = require("../users/users-model.js");
+const secret = require("../config/secret.js");
+
+//Register a new user and save to the DB
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hashedPass = bcrypt.hashSync(user.password, 12);
+  user.password = hashedPass;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "There was an error adding the user to the DB." });
+    });
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: `Welcome ${user.username}!`, token });
+      } else {
+        res
+          .status(401)
+          .json({ message: "Invalid login credentials, please try again" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "There was an error logging you in" });
+    });
+});
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "5d"
+  };
+  return jwt.sign(payload, secret.jwtSecret, options);
+}
+
+module.exports = router;
